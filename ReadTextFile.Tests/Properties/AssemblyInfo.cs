9430a8f75b08bbbf79db452475ac9615c31c5380
Kunal Chalotra <31889898+chalotrakunal@users.noreply.github.com>
2025-06-12 17:14:11 -0600

    Add MSTest project and multiline word count test

---
+++ ReadTextFile.Tests/Properties/AssemblyInfo.cs
@@ -0,0 +1,18 @@
+using System.Reflection;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ReadTextFile.Tests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ReadTextFile.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2023")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("E6F4108A-2D39-4EC3-9D08-381E4B0A5F08")]
+
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
